055d5169b72162209d46b25f9d31f2c42cf5c9c7
Cristina Roman <cristinaroman.24@campuscamara.es>
2026-02-09 10:16:35 +0100

    primer commit spring boot

---
+++ src/main/java/com/campuscamara/gestionasignaturas/GestionasignaturasApplication.java
@@ -0,0 +1,13 @@
+package com.campuscamara.gestionasignaturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionasignaturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionasignaturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campuscamara/gestionasignaturas/GestionasignaturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campuscamara.gestionasignaturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionasignaturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
